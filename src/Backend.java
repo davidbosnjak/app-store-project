1fbec2702745643772c4f8a4d3355652608133e6
davidbosnjak <davidbosnjak2@gmail.com>
2022-10-13 19:53:24 -0400

    added method declarations in backend

--- src/Backend.java
+++ src/Backend.java
@@ -1,9 +1,29 @@
 import java.io.*;
 import java.net.URL;
 import java.io.*;
+import java.util.HashMap;
 public class Backend {
-    public static void Search(String searchTerm){
+    public static String[] Search(String searchTerm){
+        //temporary so we dont have errors
+        String[] matchingEntries = {searchTerm};
+       //check if what they searched contains stuff from entries
+        return matchingEntries;
+    }
+    public static void downloadFile(String fileURL){
 
     }
+
+    public static void displayAppsFromSearchHits(String[] searchHits){
+
+    }
+
+
+
+
+
+    //database
+    static HashMap<String, String[]> database  = new HashMap<String, String[]>();
+    //might have to hardcode putting items into database on run...
+
 
 }
