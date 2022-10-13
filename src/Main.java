6d86c3ed8a929220d28720ee1e3607b73198999d
david <davidbosnjak2@gmail.com>
2022-10-13 14:49:14 -0400

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -6,5 +9,25 @@ public class Main {
         System.out.println("Test");
 
         System.out.println("hello world");
+        mainWindow();
+    }
+
+    public static void mainWindow(){
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("App Store");
+        frame.setSize(1280,720);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(null);
+        frame.add(panel);
+        JLabel titleMessage = new JLabel("Welcome to my app store");
+        titleMessage.setFont(new Font("Times", Font.BOLD,20));
+        titleMessage.setBounds(50,20,400,25);
+        panel.add(titleMessage);
+        frame.setVisible(true);
+
+
+
     }
 }
