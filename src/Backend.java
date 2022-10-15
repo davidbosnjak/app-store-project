8a2f175229f4c6fe6a40752910dcd3a8046bb4f2
davidbosnjak <davidbosnjak2@gmail.com>
2022-10-15 13:24:59 -0400

    made search method

--- src/Backend.java
+++ src/Backend.java
@@ -1,12 +1,29 @@
 import java.io.*;
 import java.net.URL;
 import java.io.*;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 public class Backend {
-    public static String[] Search(String searchTerm){
-        //temporary so we dont have errors
-        String[] matchingEntries = {searchTerm};
-       //check if what they searched contains stuff from entries
+    static final int MAX_SEARCH_TERM = 20;
+    public static HashSet<String> Search(String searchTerm, HashMap<String, String[]> database){
+        HashSet<String> matchingEntries = new HashSet();
+
+        if(searchTerm.length()>MAX_SEARCH_TERM){
+            //not gonna make a search term
+            return matchingEntries;
+        }
+
+
+        for(int i =searchTerm.length(); i>0; i--){
+            String slicedSearchTerm = searchTerm.substring(0,i);
+            for(String key: database.keySet()){
+                if(key.indexOf(slicedSearchTerm)!=-1){
+                    matchingEntries.add(key);
+                }
+            }
+        }
+
         return matchingEntries;
     }
     public static void downloadFile(String fileURL){
@@ -16,8 +33,6 @@ public class Backend {
     public static void displayAppsFromSearchHits(String[] searchHits){
 
     }
-
-
 
 
 
